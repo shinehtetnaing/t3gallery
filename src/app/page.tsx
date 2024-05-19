4a2e89b7a826d27218f6904ee572b053835a0f37
shinehtetnaing <shinehtetnaing1998@gmail.com>
2024-05-19 18:22:58 +0700

    using next image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
+import Image from "next/image";
 import { getMyImages } from "~/server/queries";
 
 export const dynamic = "force-dynamic";
@@ -18,12 +19,12 @@ async function Images() {
   const images = await getMyImages();
 
   return (
-    <div className="flex flex-wrap gap-4">
+    <div className="flex flex-wrap justify-center gap-4">
       {/* {[...images, ...images, ...images].map((image, index) => ( */}
       {images.map((image) => (
         // <div key={image.id + "-" + index} className="flex w-48 flex-col">
         <div key={image.id} className="flex w-48 flex-col">
-          <img src={image.url} alt="image" />
+          <Image src={image.url} alt="image" width={480} height={480} />
           <div>{image.name}</div>
         </div>
       ))}
